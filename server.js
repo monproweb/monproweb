24b0a943343e9257a1e23ea36770003db1b9242f
Thomas Erhel <thomas.erhel@gmail.com>
2021-06-06 01:17:01 +0200

    Set CSP helmet

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res
+        .set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
+        .send("<html><head></head><body></body></html>");
 });
 
 const httpServer = http.createServer(app);
