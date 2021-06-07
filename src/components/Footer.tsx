6d81aefabc46e9a798b8c68cf3dd18a86b87aec9
Thomas Erhel <thomas.erhel@gmail.com>
2021-06-08 00:52:44 +0200

    Update icons size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,27 +14,27 @@ const Footer = () => {
                 <Grid gridTemplateColumns="repeat(5, auto)" gridGap={3}>
                     <Box p={3} >
                         <Link mb={1} href="https://twitter.com/monproweb">
-                            <FaTwitter size={42} aria-label="Twitter" className="social" />
+                            <FaTwitter size={30} aria-label="Twitter" className="social" />
                         </Link>
                     </Box>
                     <Box p={3} >
                         <Link mb={1} href="https://facebook.com/monproweb">
-                            <FaFacebook size={42} aria-label="Facebook" className="social" />
+                            <FaFacebook size={30} aria-label="Facebook" className="social" />
                         </Link>
                     </Box>
                     <Box p={3} >
                         <Link mb={1} href="https://instagram.com/monproweb">
-                            <FaInstagram size={42} aria-label="Instagram" className="social" />
+                            <FaInstagram size={30} aria-label="Instagram" className="social" />
                         </Link>
                     </Box>
                     <Box p={3} >
                         <Link mb={1} href="https://linkedin.com/company/monproweb">
-                            <FaLinkedinIn size={42} aria-label="Linkedin" className="social" />
+                            <FaLinkedinIn size={30} aria-label="Linkedin" className="social" />
                         </Link>
                     </Box>
                     <Box p={3} >
                         <Link mb={1} href="https://github.com/monproweb">
-                            <FaGithub size={42} aria-label="GitHub" className="social" />
+                            <FaGithub size={30} aria-label="GitHub" className="social" />
                         </Link>
                     </Box>
                 </Grid>
