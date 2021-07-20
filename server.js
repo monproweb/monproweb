ab947a97ea3b8a502c2c26f7641b1c6df8f707b8
Thomas Erhel <thomas.erhel@gmail.com>
2021-07-20 12:32:49 +0200

    Add maxAge

--- server.js
+++ server.js
@@ -40,7 +40,9 @@ app.use(
 
 app.use(compression())
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'build', {
+  maxAge: '5000'
+})))
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
