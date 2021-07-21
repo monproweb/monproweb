83960b4b72729549981d1bfb6a63a0a8867dd5cc
Thomas Erhel <thomas.erhel@gmail.com>
2021-07-21 23:58:14 +0200

    Import Footer in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="Mpw-app">
       <NavBar />
+      <Footer />
     </div>
   );
 }
