e666e30a36ad422da76eb8572cbf2e32a768c589
Thomas Erhel <thomas.erhel@gmail.com>
2021-11-22 19:05:47 +0100

    Add Header + 🚂🌈

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import GPLv3 from './images/gplv3-88x31.png'
 import Particles from 'react-tsparticles'
 import particlesOptions from './particles.json'
 import { ISourceOptions } from 'tsparticles'
-import {ThemeProvider, BaseStyles, Box, Heading, Link, Text, Button} from '@primer/components'
+import {ThemeProvider, BaseStyles, Box, Heading, Link, Text, Button, Header, StyledOcticon, Avatar} from '@primer/components'
+import {InfinityIcon} from '@primer/styled-octicons'
 
 class App extends Component {
     handleClick = () => {
@@ -20,11 +21,23 @@ class App extends Component {
     render() {
         return (
                 <ThemeProvider colorMode="auto">
-                      <BaseStyles>
+            <BaseStyles>
+<Header>
+  <Header.Item>
+    <Header.Link href="/" fontSize={2}>
+      <StyledOcticon icon={InfinityIcon} size={32}  sx={{mr: 2}} />
+      <span>Mon Pro Web</span>
+    </Header.Link>
+  </Header.Item>
+  <Header.Item full>Menu</Header.Item>
+  <Header.Item mr={0}>
+    <Avatar src="/logo192.png" size={20} square alt="@monproweb" />
+  </Header.Item>
+</Header>
                     <Box m={4}>
                 <Box className="App">
                     <Particles options={particlesOptions as ISourceOptions}/>
-                    <header className="App-header">
+                  <header className="App-header">
                             <Logo title="logo" className="App-logo" />
                                 <Box p={3} bg='canvas.default'>
                                     <Heading as='h1' sx={{ mb: 2 }}>Bonjour! 👋</Heading>
@@ -36,9 +49,11 @@ class App extends Component {
                             <Button onClick={this.handleClick}>
                                 🔥 Citation
                             </Button>
-                        </Box>
-                        <Box p={3}>
+                      </Box>
+                      <Box p={3}>
+                        <Link sx={{mb: 1}} href="/about/javascript.html" target="_blank" rel="noopener noreferrer">
                             <img src={GPLv3} alt="Free as in Freedom" />
+                        </Link>
                         </Box>
                                 </Box>
                     </header>
