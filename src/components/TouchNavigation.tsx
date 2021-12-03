684c73c3a4ebc005735f1e449d6e2c4f329bd2ef
Thomas Erhel <thomas.erhel@gmail.com>
2021-12-03 18:25:51 +0100

    Resize search icon

--- src/components/TouchNavigation.tsx
+++ src/components/TouchNavigation.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Header, StyledOcticon, TextInput, Tooltip } from '@primer/components'
+/* eslint-disable primer-react/no-system-props */
+import { Avatar, CircleOcticon, Header, StyledOcticon, TextInput, Tooltip } from '@primer/components'
 import { SearchIcon, SignInIcon } from '@primer/styled-octicons'
 import React, { Component } from 'react'
 
@@ -10,16 +11,7 @@ class TouchNavigation extends Component {
     <Header.Link href="/" fontSize={2}>
       <Avatar src="/logo192.png" size={32} sx={{mr: 2}} alt="@monproweb" />
     </Header.Link>
-    <TextInput
-    icon={SearchIcon}
-    aria-label="Search"
-    name="search"
-    placeholder="Rechercher sur Monproweb"
-    autoComplete="off"
-    contrast
-    block
-    sx={{width: 290}}
-  />
+    <CircleOcticon icon={SearchIcon} size={24} />
   </Header.Item>
   <Header.Item mr={0}>
     <Tooltip aria-label="Se connecter" direction="sw"><StyledOcticon icon={SignInIcon} size={20}  sx={{mr: 2}} /></Tooltip>
