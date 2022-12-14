c6a1430621e4cb5393626d06bd5f1cf0bc66f7b2
Thomas Erhel <thomas.erhel@gmail.com>
2022-12-14 14:30:24 +0100

    ✌️ Practice Makes Perfect

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,27 @@ import Ethereum from './Ethereum'
 import metamask from '../images/metamask.svg'
 
 function Layout() {
+
+  const handleClick = (anchor) => () => {
+    const id = `${anchor}-section`;
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
   return (
     <>
       <Header>
         <Header.Item>
           <Header.Link href="/" fontSize={2}>
             <Avatar src={logo} size={32} sx={{ mr: 2 }} alt="@monproweb" />
-            <span>Mon Pro Web</span>
           </Header.Link>
         </Header.Item>
         <Header.Item>
-          <Header.Link href="/projects">Projects</Header.Link>
+          <Header.Link href="#about" onClick={handleClick("about")}>About</Header.Link>
         </Header.Item>
         <Header.Item full>
           <Header.Link href="/contact">Contact</Header.Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Box, PageLayout, Heading, Text, Avatar } from '@primer/react'
 import nft from '../images/nft.png'
 import cat from '../images/cat.png'
+import rsm from '../images/rsm.png'
 import ship from '../images/ship.png'
 import '../styles/Home.scss'
 
@@ -16,7 +17,7 @@ function Home() {
             noOfLines={1}
           >
             <Box p={3}>
-              <Avatar src={nft} size="xl" />
+              <Avatar src={nft} size="300" />
             </Box>
             <Box p={3}>
               <Heading sx={{ fontSize: 42, mb: 2 }}>I build Web and mobile applications.</Heading>
@@ -24,7 +25,7 @@ function Home() {
             </Box>
         </PageLayout.Header>
   <PageLayout.Content aria-label="content">
-          <Box label="Content" maxWidth="1280px" minHeight="100vh" sx={{ fontSize: 20, mb: 2 }}>
+          <Box label="Content" maxWidth="1280px" minHeight="100vh" id="about-section">
             <Box marginBottom={2} className='Avatar'>
               <Avatar src={cat} size="100" />
               </Box>
@@ -33,6 +34,9 @@ function Home() {
               Monproweb is a startup that specializes in building web and mobile applications with a focus on sustainability and the use of Ethereum blockchain technology. Our team of experienced developers utilizes the React framework to create intuitive and user-friendly apps that deliver powerful functionality.
               </Text>
             </Box>
+            <Box marginBottom={2} className='Avatar'>
+              <Avatar src={rsm} size="100" />
+              </Box>
             <Box marginBottom={2}>
             <Text>
               As an eco-friendly company, we prioritize the use of clean and renewable technologies in all aspects of our work. This not only benefits the environment, but also allows us to provide our clients with cutting-edge solutions that are built to last.
