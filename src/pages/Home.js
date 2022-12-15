e36b2d4e50560da763a5ca41b1f5525dc6c7c723
Thomas Erhel <thomas.erhel@gmail.com>
2022-12-15 21:47:59 +0100

    Format code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,23 +5,22 @@ function Home() {
   return (
     <>
 <PageLayout>
-        <PageLayout.Header aria-label="header">
-          
-        </PageLayout.Header>
-        <PageLayout.Content aria-label="content">
-            <Box p={3}>
-              <Avatar src={nft} size="300" alt='Non Fungible Token' />
-            </Box>
-            <Box p={3}>
-              <Heading sx={{ fontSize: 42, mb: 2 }}>I build Web and mobile applications.</Heading>
-              </Box>          
+  <PageLayout.Content aria-label="content">
+    <Box p={3}>
+      <Avatar src={nft} size="300" alt='Non Fungible Token' />
+    </Box>
+    <Box p={3}>
+      <Heading sx={{ fontSize: 42, mb: 2 }}>I build Web and mobile applications.</Heading>
+    </Box>          
   </PageLayout.Content>
   <PageLayout.Footer aria-label="footer">
-          <Box label="Footer" height={64}>
-            <Text></Text>
-          </Box>
+    <Box label="Footer" height={64}>
+      <Text>
+              
+      </Text>
+    </Box>
   </PageLayout.Footer>
-</PageLayout>  
+</PageLayout>
     </>
   );
 }
