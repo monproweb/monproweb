1241aaad6afcaaab1ffe4260e38b77f16c9648c2
Thomas Erhel <thomas.erhel@gmail.com>
2022-12-21 05:49:24 +0100

    Animate metamask logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import * as React from "react";
 import { Outlet } from "react-router-dom";
-import { Header, Avatar } from '@primer/react'
-import logo from '../logo.svg'
-import Ethereum from './Ethereum'
-import metamask from '../images/metamask.svg'
+import { Header, Avatar } from '@primer/react';
+import logo from '../logo.svg';
+import Ethereum from './Ethereum';
+import metamask from '../images/metamask.svg';
 
 function Layout() {
+  const [rotation, setRotation] = React.useState(0);
+
+  React.useEffect(() => {
+    const handleMouseMove = (event) => {
+      const rect = event.target.getBoundingClientRect();
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+      const radians = Math.atan2(y, x);
+      const degrees = (radians * 180) / Math.PI;
+      setRotation(degrees);
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <>
       <Header>
@@ -25,7 +44,13 @@ function Layout() {
           <Ethereum />
         </Header.Item>
         <Header.Item mr={0}>
-          <Avatar src={metamask} size={20} square alt="@metamask" />
+          <Avatar
+            src={metamask}
+            size={20}
+            square
+            alt="@metamask"
+            style={{ transform: `rotate(${rotation}deg)` }}
+          />
         </Header.Item>
       </Header>
       <Outlet />
