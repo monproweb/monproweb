ba0a40125f803f4582f86f3a66a906354c214c9c
Thomas Erhel <thomas.erhel@gmail.com>
2022-12-25 21:19:18 +0100

    💚 Remove analytics and Ethereum component

--- src/index.tsx
+++ src/index.tsx
@@ -6,26 +6,10 @@ import {ThemeProvider, BaseStyles} from '@primer/react'
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const firebaseConfig = {
-    apiKey: 'AIzaSyBcxpWbGYVFvVJM4NZmG8Uk-qKjkbfhBco',
-    authDomain: 'mon-pro-web.firebaseapp.com',
-    databaseURL: 'https://mon-pro-web-default-rtdb.firebaseio.com',
-    projectId: 'mon-pro-web',
-    storageBucket: 'mon-pro-web.appspot.com',
-    messagingSenderId: '525128482932',
-    appId: '1:525128482932:web:eb3de96c568a2ce87b5121',
-    measurementId: 'G-4GV65QSVDM',
-}
-
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
 
 root.render(
   <React.StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Box, PageLayout, Heading, Text } from '@primer/react';
-import Ethereum from '../components/Ethereum';
-import { useMemo, useCallback } from 'react';
-import React from 'react';
-
-const MemoizedEthereum = React.memo(Ethereum);
 
 function Home() {
-  const expensiveCalculation = useMemo(() => {
-  }, []);
-
-  const callback = useCallback(() => {
-  }, []);
-
   return (
     <>
       <PageLayout>
         <PageLayout.Content aria-label="content">
           <Box p={3}>
             <Heading sx={{ fontSize: 42, mb: 2 }}>I build Web and mobile applications.</Heading>
-          </Box>
-          <Box p={3}>
-            <MemoizedEthereum callback={callback} />
           </Box>
         </PageLayout.Content>
         <PageLayout.Footer aria-label="footer">
