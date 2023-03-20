4a1fdb8904027b414fc77950dd823f2ed01beecf
Thomas Erhel <thomas.erhel@gmail.com>
2023-03-20 21:50:24 +0100

    Fix: Sync SwitchTheme with auto color mode

--- src/components/SwitchTheme.tsx
+++ src/components/SwitchTheme.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTheme, Button } from '@primer/react'
 import { SunIcon, MoonIcon } from '@primer/octicons-react'
 
 const SwitchTheme = () => {
-    const { setColorMode } = useTheme()
+    const { setColorMode, resolvedColorMode } = useTheme()
     const [isNight, setIsNight] = useState(false)
+
+    useEffect(() => {
+        setIsNight(resolvedColorMode === 'night')
+    }, [resolvedColorMode])
 
     const handleClick = () => {
         if (isNight) {
