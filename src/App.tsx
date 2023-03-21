f7f5142c76a865bc59952fc81df06396e0b8ca33
Thomas Erhel <thomas.erhel@gmail.com>
2023-03-21 06:55:38 +0100

    Format App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,45 @@
-import * as React from "react";
-import { Routes, Route } from "react-router-dom";
-import Layout from "components/Header";
-import Home from "pages/Home";
-import { Spinner } from '@primer/react';
+import * as React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Layout from 'components/Header'
+import Home from 'pages/Home'
+import { Spinner } from '@primer/react'
 
-const About = React.lazy(() => import("./pages/About"));
-const Privacy = React.lazy(() => import("./pages/Privacy"));
-const Terms = React.lazy(() => import("./pages/Terms"));
-const NoMatch = React.lazy(() => import("./pages/NoMatch"));
+const About = React.lazy(() => import('./pages/About'))
+const Privacy = React.lazy(() => import('./pages/Privacy'))
+const Terms = React.lazy(() => import('./pages/Terms'))
+const NoMatch = React.lazy(() => import('./pages/NoMatch'))
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route
-          path="about"
-          element={
-            <React.Suspense fallback={<Spinner />}>
-              <About />
-            </React.Suspense>
-          } 
-        />
-        <Route
-          path="privacy"
-          element={
-            <React.Suspense fallback={<Spinner />}>
-              <Privacy />
-            </React.Suspense>
-          } 
-        />
-        <Route
-          path="terms"
-          element={
-            <React.Suspense fallback={<Spinner />}>
-              <Terms />
-            </React.Suspense>
-          } 
-        />
-        <Route path="*" element={<NoMatch />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route
+                    path="about"
+                    element={
+                        <React.Suspense fallback={<Spinner />}>
+                            <About />
+                        </React.Suspense>
+                    }
+                />
+                <Route
+                    path="privacy"
+                    element={
+                        <React.Suspense fallback={<Spinner />}>
+                            <Privacy />
+                        </React.Suspense>
+                    }
+                />
+                <Route
+                    path="terms"
+                    element={
+                        <React.Suspense fallback={<Spinner />}>
+                            <Terms />
+                        </React.Suspense>
+                    }
+                />
+                <Route path="*" element={<NoMatch />} />
+            </Route>
+        </Routes>
+    )
 }
