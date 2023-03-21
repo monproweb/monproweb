b1b54418499a08cf5d644ecbe05b3581ee727952
Thomas Erhel <thomas.erhel@gmail.com>
2023-03-21 21:00:06 +0100

    Add animations to home page

--- src/components/Home/Hero/CallToAction.tsx
+++ src/components/Home/Hero/CallToAction.tsx
@@ -1,30 +1,49 @@
 import { Box, Button, Link } from '@primer/react'
 import { Link as RouterLink } from 'react-router-dom'
 import { RocketIcon } from '@primer/octicons-react'
+import { useSpring, animated } from 'react-spring'
 
 const TWITTER_URL = 'https://twitter.com/ThomasErhel'
 const ABOUT_URL = '/about'
 
 export default function CallToAction() {
+    const buttonAnimation = useSpring({
+        from: { opacity: 0, transform: 'translateX(-30px)' },
+        to: { opacity: 1, transform: 'translateX(0px)' },
+        config: { duration: 1000 },
+        delay: 300,
+    })
+
+    const linkAnimation = useSpring({
+        from: { opacity: 0, transform: 'translateX(30px)' },
+        to: { opacity: 1, transform: 'translateX(0px)' },
+        config: { duration: 1000 },
+        delay: 600,
+    })
+
     return (
         <Box p={3}>
-            <Button
-                leadingIcon={RocketIcon}
-                aria-label="Get in touch"
-                variant="primary"
-                onClick={() => {
-                    window.open(TWITTER_URL, '_blank')
-                }}
-            >
-                Get in touch
-            </Button>
-            <Link
-                to={ABOUT_URL}
-                as={RouterLink}
-                aria-label="Learn more about Monproweb"
-            >
-                Learn more
-            </Link>
+            <animated.div style={buttonAnimation}>
+                <Button
+                    leadingIcon={RocketIcon}
+                    aria-label="Get in touch"
+                    variant="primary"
+                    onClick={() => {
+                        window.open(TWITTER_URL, '_blank')
+                    }}
+                >
+                    Get in touch
+                </Button>
+            </animated.div>
+            <animated.div style={linkAnimation}>
+                <Link
+                    to={ABOUT_URL}
+                    as={RouterLink}
+                    aria-label="Learn more about Monproweb"
+                >
+                    Learn more
+                </Link>
+            </animated.div>
         </Box>
     )
 }
--- src/components/Home/Hero/ProgrammingImage.tsx
+++ src/components/Home/Hero/ProgrammingImage.tsx
@@ -1,16 +1,25 @@
 import { Box } from '@primer/react'
+import { useSpring, animated } from 'react-spring'
 import Programming from '../../../images/programming.svg'
 
 export default function ProgrammingImage() {
+    const imageAnimation = useSpring({
+        from: { opacity: 0, transform: 'scale(0.8)' },
+        to: { opacity: 1, transform: 'scale(1)' },
+        config: { duration: 1000 },
+    })
+
     return (
         <Box p={3}>
-            <img
-                className="programming"
-                src={Programming}
-                alt="programming"
-                width="300px"
-                height="300px"
-            />
+            <animated.div style={imageAnimation}>
+                <img
+                    className="programming"
+                    src={Programming}
+                    alt="programming"
+                    width="300px"
+                    height="300px"
+                />
+            </animated.div>
         </Box>
     )
 }
