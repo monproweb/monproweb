b0f59c6e923d92b7b6c941173d0d0b573addf5ec
Thomas Erhel <thomas.erhel@gmail.com>
2023-03-21 09:44:06 +0100

    Refactor About component for readability

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,40 @@ import rsm from '../images/rsm.webp'
 import ship from '../images/ship.webp'
 import mind from '../images/mind.webp'
 
+const sections = [
+    {
+        image: cat,
+        alt: 'Cat in space',
+        text: 'Monproweb is a startup that specialized in building web and mobile applications with a focus on sustainability and the use of Ethereum blockchain technology. Our team of experienced developers utilizes the React library to create intuitive and user-friendly apps that deliver powerful functionality.',
+    },
+    {
+        image: rsm,
+        alt: 'Positive change ambassador badge from RSM',
+        text: 'As an eco-friendly company, we prioritize the use of clean and renewable technologies in all aspects of our work. This not only benefits the environment, but also allows us to provide our clients with cutting-edge solutions that are built to last.',
+    },
+    {
+        image: ship,
+        alt: 'A boat with Ethereum sail',
+        text: 'In addition to our focus on sustainability, we also have extensive experience with Ethereum blockchain technology. This allows us to build applications that are secure, transparent, and highly scalable. Whether you need a simple web application or a complex decentralized application, we have the skills and expertise to deliver the solution you need.',
+    },
+    {
+        image: mind,
+        alt: 'ChatGPT mind',
+        text: 'At Monproweb, we pride ourselves on our ability to deliver high-quality web and mobile solutions that meet the unique needs of each of our clients. Contact us today to discuss your project and learn more about how we can help bring your vision to life.',
+    },
+]
+
+function AboutSection({ image, alt, text }) {
+    return (
+        <Box>
+            <Box sx={{ textAlign: 'center' }}>
+                <Avatar src={image} size={100} alt={alt} />
+            </Box>
+            <Text>{text}</Text>
+        </Box>
+    )
+}
+
 export default function About() {
     return (
         <>
@@ -25,65 +59,9 @@ export default function About() {
                     },
                 }}
             >
-                <Box>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <Avatar src={cat} size={100} alt="Cat in space" />
-                    </Box>
-                    <Text>
-                        Monproweb is a startup that specialized in building web
-                        and mobile applications with a focus on sustainability
-                        and the use of Ethereum blockchain technology. Our team
-                        of experienced developers utilizes the React library to
-                        create intuitive and user-friendly apps that deliver
-                        powerful functionality.
-                    </Text>
-                </Box>
-                <Box>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <Avatar
-                            src={rsm}
-                            size={100}
-                            alt="Positive change ambassador badge from RSM"
-                        />
-                    </Box>
-                    <Text>
-                        As an eco-friendly company, we prioritize the use of
-                        clean and renewable technologies in all aspects of our
-                        work. This not only benefits the environment, but also
-                        allows us to provide our clients with cutting-edge
-                        solutions that are built to last.
-                    </Text>
-                </Box>
-                <Box>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <Avatar
-                            src={ship}
-                            size={100}
-                            alt="A boat with Ethereum sail"
-                        />
-                    </Box>
-                    <Text>
-                        In addition to our focus on sustainability, we also have
-                        extensive experience with Ethereum blockchain
-                        technology. This allows us to build applications that
-                        are secure, transparent, and highly scalable. Whether
-                        you need a simple web application or a complex
-                        decentralized application, we have the skills and
-                        expertise to deliver the solution you need.
-                    </Text>
-                </Box>
-                <Box>
-                    <Box sx={{ textAlign: 'center' }}>
-                        <Avatar src={mind} size={100} alt="ChatGPT mind" />
-                    </Box>
-                    <Text>
-                        At Monproweb, we pride ourselves on our ability to
-                        deliver high-quality web and mobile solutions that meet
-                        the unique needs of each of our clients. Contact us
-                        today to discuss your project and learn more about how
-                        we can help bring your vision to life.
-                    </Text>
-                </Box>
+                {sections.map((section, index) => (
+                    <AboutSection key={index} {...section} />
+                ))}
             </Box>
         </>
     )
