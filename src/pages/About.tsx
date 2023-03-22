7521d0e7ccf4ba7ccd3dc80e5d2cb32bf6b2dd94
Thomas Erhel <thomas.erhel@gmail.com>
2023-03-22 03:30:26 +0100

    Update about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -63,7 +63,15 @@ function AboutSection({ image, alt, text, index }) {
 
 export default function About() {
     return (
-        <>
+        <Box
+            sx={{
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
             <Heading sx={{ fontSize: 42, mb: 2 }}>About MonProWeb</Heading>
             <Box
                 sx={{
@@ -86,6 +94,6 @@ export default function About() {
                     <AboutSection index={undefined} key={index} {...section} />
                 ))}
             </Box>
-        </>
+        </Box>
     )
 }
